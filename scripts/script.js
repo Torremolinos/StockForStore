6371743f86e6836ce8f18564ad65f509c44a3c28
Torremolinos <adrian_2054@hotmail.com>
2023-10-20 00:20:00 +0200

    Añado Eliminar pero tiene fallos

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,7 @@
+/**
+ * @Autor: Adrian Iglesias Riño.
+ * @Github: https://github.com/Torremolinos/StockForStore
+ */
 document.addEventListener("DOMContentLoaded", () => {
   const actualizar = document.getElementById("actualizar");
   const buscar = document.getElementById("buscar");
@@ -8,7 +12,6 @@ document.addEventListener("DOMContentLoaded", () => {
     { id: 1, nombre: "copos de avena", cantidad: 50, precio: 1 },
     { id: 2, nombre: "solomillos de salmon", cantidad: 30, precio: 15 },
     { id: 3, nombre: "nueces", cantidad: 20, precio: 3 },
-    { id: 4, nombre: "foskitos", cantidad: 15, precio: 6 },
   ];
   /*const nuevo = {id: 5, nombre: "maiz", cantidad: 60, precio: 40 };
   console.log(typeof(nuevo));
@@ -39,39 +42,67 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(`Precio Total: ${precioTotal()}`);
     console.log("-------------");
   });
-  
+
   const eliminarItem = () => {
-    
-
-
+    const nombreProducto = document
+      .getElementById("nombre")
+      .value.trim()
+      .toLowerCase();
+    for (let i = 0; i < inventario.length; i++) {
+      const element = inventario[i];
+      if (inventario[i].nombre === nombreProducto) {
+        inventario.splice(i, 1);
+      }
+    }
+    //el problema por lo que veo es que cuando borro el objeto con id inferior al maximo
+    //cuando creo otro se iguala al ultimo mas alto. no entiendo quiero arreglarlo pero 
+    //me esta volviendo loco.
   };
+
+  eliminar.addEventListener("click", () => {
+    eliminarItem();
+    inventario.forEach((producto) => {
+      console.log("ID: " + producto.id)});
+  });
   const agregarItem = () => {
     let productoUltimoId = 1;
     const regex = /^[a-zA-Z\s]*[a-zA-Z][a-zA-Z\s]*$/;
-    const nombreProducto = document.getElementById("nombre").value.trim().toLowerCase();
-    const cantidadProducto = parseFloat(document.getElementById("cantidad").value);
+    const nombreProducto = document
+      .getElementById("nombre")
+      .value.trim()
+      .toLowerCase();
+    const cantidadProducto = parseFloat(
+      document.getElementById("cantidad").value
+    );
     const precioProducto = parseFloat(document.getElementById("precio").value);
     inventario.forEach((producto) => {
       producto.id;
       productoUltimoId++;
       console.log(`${productoUltimoId}`);
     });
-    if (nombreProducto == "" || !regex.test(nombreProducto) || nombreProducto==" " ||cantidadProducto < 0 || isNaN(cantidadProducto) || precioProducto < 0 || isNaN(precioProducto)){
+    if (
+      nombreProducto == "" ||
+      !regex.test(nombreProducto) ||
+      nombreProducto == " " ||
+      cantidadProducto < 0 ||
+      isNaN(cantidadProducto) ||
+      precioProducto < 0 ||
+      isNaN(precioProducto)
+    ) {
       alert(`Inserta correctamente los datos`);
       return;
     }
-    
-    const newItem = 
-      {
-        id: productoUltimoId,
-        nombre: nombreProducto,
-        cantidad: cantidadProducto,
-        precio: precioProducto,
-      };
+
+    const newItem = {
+      id: productoUltimoId,
+      nombre: nombreProducto,
+      cantidad: cantidadProducto,
+      precio: precioProducto,
+    };
     inventario.push(newItem);
-    document.getElementById("nombre").value="";
-    document.getElementById("cantidad").value="";
-    document.getElementById("precio").value="";
+    document.getElementById("nombre").value = "";
+    document.getElementById("cantidad").value = "";
+    document.getElementById("precio").value = "";
   };
 
   agregar.addEventListener("click", () => {
@@ -87,6 +118,19 @@ document.addEventListener("DOMContentLoaded", () => {
       fila.innerHTML = `<td>${producto.id}</td><td>${producto.nombre}</td><td>${producto.cantidad}</td><td>${producto.precio}</td>`;
       tablaContainer.appendChild(fila);
     });
+    /* aqui con while... function actualizacionBody() {
+      const tablaContainer = document.getElementById("body");
+      tablaContainer.innerHTML = "";
+    
+      let i = 0;
+      while (i < inventario.length) {
+        const producto = inventario[i];
+        const fila = document.createElement("tr");
+        fila.innerHTML = `<td>${producto.id}</td><td>${producto.nombre}</td><td>${producto.cantidad}</td><td>${producto.precio}</td>`;
+        tablaContainer.appendChild(fila);
+        i++;
+      }
+    }*/
   }
   function actualizacionPie() {
     const tablaContainer = document.getElementById("pie");
