06a4ba25afdb0fe2ea10fa54ccd07d47687a8d0e
Torremolinos <adrian_2054@hotmail.com>
2023-11-02 21:24:02 +0100

    Metodo actualizar empezando

--- scripts/actualizar.js
+++ scripts/actualizar.js
@@ -1,7 +1,17 @@
 import { inventario } from "./inventario.js";
 // Cuando se confirman los cambios, actualiza el producto
-export function guardarCambios() {
- 
-  }
-  
+export const guardarCambios = (producto) => {
+  const ventanaActualizacion = window.open(
+    "actualizarProducto.html",
+    "Actualización de Producto",
+    "width=400,height=300"
+  );
+  // Comunica los datos del producto a la ventana emergente
+  ventanaActualizacion.postMessage(producto, "*");
+};
+const updateButton = document.createElement("button");
+updateButton.innerText = "Actualizar";
+updateButton.addEventListener("click", () => {
+  abrirFormularioActualizacion(item); // Pasa el objeto del producto a la ventana emergente
+});
 // window.location.href ='salir.html';
